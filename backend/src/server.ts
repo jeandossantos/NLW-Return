07d7cce6585c8fe8f85a200d53d4b66c70fc9f3e
= <=>
2022-05-10 17:20:40 -0300

    ajustes pra deploy 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,10 @@ import { routes } from './routes';
 import cors from 'cors';
 
 const app = express();
+const port = 3001;
 
 app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => console.log('Back-end is running on port 3001...'));
+app.listen(port, () => console.log('Back-end is running on port 3001...'));
